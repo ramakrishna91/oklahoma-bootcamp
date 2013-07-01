6e8ba5463218136003b00fe35ad7d16f7e932061
Satya P <ramakrishna91@ou.edu>
2013-07-01 14:32:38 -0500

    Added some super important stuff

--- add_numb.py
+++ add_numb.py
@@ -3,6 +3,7 @@ from  adding import add_two, add_three
 def main():
     print ("2+2 = ", add_three(2,2,2))
     print ("2+3 = ", add_two(2,3))
-
+    print ("Hello Oklahoma")
+    
 if__name == "__main__"
     main()
